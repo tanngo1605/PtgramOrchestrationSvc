2bde45007587bda9ce913918da2dd3faa07ba1c3
tanngo1605 <50272836+tanngo1605@users.noreply.github.com>
2023-10-15 01:03:10 -0700

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.json({ msg: "Hello", username: req.username });
+  res.json({ msg: "Hello", userInfo: req.userInfo });
 });
 
 module.exports = router;
